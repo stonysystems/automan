9598968567c86e1c175d7c35c78f6b90edd19fde
tizhou <tizhou98@gmail.com>
2024-07-20 22:33:21 -0400

    update test_printer to work with the newest AST

--- test/test_printer.ml
+++ test/test_printer.ml
@@ -7,27 +7,43 @@ let print_position outx lexbuf =
   fprintf outx "%s:%d:%d" pos.pos_fname
     pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
-let parse_with_error lexbuf =
-  try Parser.file_level Lexer.lexeme lexbuf with
+let parse_dafny_with_error lexbuf =
+  try DafnyParser.dafny Lexer.lexeme lexbuf with
   | Lexer.SyntaxError msg ->
     fprintf stderr "%a: %s\n" print_position lexbuf msg;
-    None
-  | Parser.Error ->
+    exit (-1)
+  | DafnyParser.Error ->
     fprintf stderr "%a: syntax error\n"  print_position lexbuf;
     exit (-1)
 
-let rec parse_and_print lexbuf =
-  match parse_with_error lexbuf with
-  | Some x ->
-    printf "%s\n" Printer.PrettyPrinter.(FileLevel.print_t x);
-    parse_and_print lexbuf
-  | None -> ()
+let parse_annotations_with_error lexbuf =
+  try AnnotationParser.toplevel Lexer.lexeme lexbuf with
+  | Lexer.SyntaxError msg ->
+    fprintf stderr "%a: %s\n" print_position lexbuf msg;
+    exit (-1)
+  | AnnotationParser.Error ->
+    fprintf stderr "%a: syntax error\n" print_position lexbuf;
+    exit (-1)
+
+let parse_dafny_and_print lexbuf =
+  let x = parse_dafny_with_error lexbuf in
+  printf "%s\n" (Printer.PrettyPrinter.print_t x)
+
+let parse_annotations_and_print lexbuf =
+  let x = parse_annotations_with_error lexbuf in
+  printf "%s\n" (Syntax.Annotation.show_toplevel_t x)
 
 let loop filename () =
   let inx = In_channel.create filename in
   let lexbuf = Lexing.from_channel inx in
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-  parse_and_print lexbuf;
+  if Filename.check_suffix filename ".dfy" then
+    parse_dafny_and_print lexbuf
+  else if Filename.check_suffix filename ".automan" then
+    parse_annotations_and_print lexbuf
+  else
+    ();
+  parse_dafny_and_print lexbuf;
   In_channel.close inx
 
 let () =
