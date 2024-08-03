494608459cd2e2e1a8b80157f7c41504a340590f
tizhou <tizhou98@gmail.com>
2024-08-02 20:32:24 -0400

    translator: update dataTracker and translaterCommon

--- lib/dataTracker.ml
+++ lib/dataTracker.ml
@@ -20,7 +20,7 @@ end ;;
 module ExprMap = Map.Make(Expr)
 
 class data_tracker = 
-object 
+object (self)
   val mutable table         = ExprMap.empty
   val mutable end_point     = TranslatorCommon.expr_blank
   val mutable data_update   = TranslatorCommon.expr_blank
@@ -30,10 +30,6 @@ object
   method set_end_point e    = end_point <- e
   method set_data_update e  = data_update <- e
   method set_this e         = this <- e
-
-  method add_to_table 
-    (k : AST.Prog.expr_t) (v : data_tracker) = 
-    ExprMap.add k v table
 
   method copy = 
     let new_tracker = new data_tracker in 
@@ -48,6 +44,12 @@ object
     new_tracker#set_this this;
     new_tracker
 
+  method add_helper 
+    (k : AST.Prog.expr_t) (v : data_tracker) : unit = 
+    match (ExprMap.mem k table) with
+    | true -> assert false (* checking *)
+    | false -> table <- ExprMap.add k v table
+
   method is_end_point_filled = TranslatorCommon.is_expr_neq end_point
 
   method is_data_update_filled = 
@@ -56,17 +58,114 @@ object
   method query_member 
     (prefix_list : AST.Prog.expr_t list)
     (suffix_list : AST.Prog.expr_t list) : AST.Prog.expr_t = 
-    let prefix_list, suffix_list = begin
-      let suffix_list = NonEmptyList.coerce suffix_list in
-      let suffix_list, h = NonEmptyList.unsnoc suffix_list in
-      let prefix_list = prefix_list @ [h] in
-      prefix_list, suffix_list
-    end in
+    let prefix_list, suffix_list = 
+      TranslatorCommon.move_one_expr_from_suffix_to_prefix 
+        prefix_list suffix_list in
     match suffix_list with
-    | [] -> end_point (* Should check of the end_point has been assigned before *)
-    | h :: _ -> begin
-        let c1 
+    | [] -> begin
+        (* checking *)
+        assert (TranslatorCommon.is_expr_n_blank end_point);
+        end_point
       end
+    | h :: _ -> begin
+        let prefix_expr = 
+          TranslatorCommon.convert_expr_lst_to_dot_expr prefix_list in
+        (* checking *)
+        assert (
+          (TranslatorCommon.is_expr_blank this) ||
+          (TranslatorCommon.is_expr_eq this prefix_expr)
+        );
+        this <- prefix_expr;
+        match ExprMap.find_opt h table with
+        | Some entry -> entry#query_member prefix_list suffix_list
+        | None -> assert false
+      end
+
+  method query_member_wrapper suffix_list = 
+    self#query_member [] suffix_list
+
+  method add 
+    (prefix_list : AST.Prog.expr_t list)
+    (suffix_list : AST.Prog.expr_t list)
+    (value : AST.Prog.expr_t) : unit =
+    let prefix_list, suffix_list = 
+      TranslatorCommon.move_one_expr_from_suffix_to_prefix 
+        prefix_list suffix_list in 
+    let prefix_expr = 
+      TranslatorCommon.convert_expr_lst_to_dot_expr prefix_list in
+    match suffix_list with
+    | [] -> begin
+        (* checking *)
+        assert (TranslatorCommon.is_expr_n_blank end_point);
+        assert (TranslatorCommon.is_expr_blank this);
+        this <- prefix_expr;
+        end_point <- value;
+      end
+    | h :: _ -> begin
+        (* checking *)
+        assert (
+          (TranslatorCommon.is_expr_blank this) ||
+          (TranslatorCommon.is_expr_eq this prefix_expr)
+        );
+        this <- prefix_expr;
+        match ExprMap.find_opt h table with
+        | Some entry -> entry#add prefix_list suffix_list value
+        | None -> begin
+          let new_entry = new data_tracker in
+          new_entry#add prefix_list suffix_list value;
+          self#add_helper h new_entry
+        end
+    end
+
+  method add_wrapper suffix_list value = 
+    self#add [] suffix_list value
+
+  method add_data_update 
+  (prefix_list  : AST.Prog.expr_t list)
+  (suffix_list  : AST.Prog.expr_t list)
+  (value        : AST.Prog.expr_t)
+  (root_entry   : data_tracker)
+  (s            : AST.Prog.expr_t)
+  (s'           : AST.Prog.expr_t) : unit = 
+  let prefix_list, suffix_list = 
+  TranslatorCommon.move_one_expr_from_suffix_to_prefix 
+    prefix_list suffix_list in 
+  let prefix_expr = 
+    TranslatorCommon.convert_expr_lst_to_dot_expr prefix_list in
+  let rec replace_dot_expr_starts_with_s' 
+    (e : AST.Prog.expr_t) : AST.Prog.expr_t = 
+    if (TranslatorCommon.ExprTypeHelper.is_expr_tp_aug_dot e) then
+      let e_lst = TranslatorCommon.convert_dot_expr_to_expr_lst e in
+      let e_lst = NonEmptyList.coerce e_lst in 
+      let rest, h = NonEmptyList.unsnoc e_lst in
+      match (TranslatorCommon.is_expr_eq h s') with
+      | true -> root_entry#query_member_wrapper (s :: rest)
+      | false -> e
+    else if (TranslatorCommon.ExprTypeHelper.is_expr_tp_data_update e) then
+      e
+    else
+      e
+  in
+  match suffix_list with
+  | [] -> begin
+    assert (TranslatorCommon.is_expr_blank data_update);
+    this <- prefix_expr;
+  end
+  | h :: _ -> begin 
+    (* checking *)
+    assert (
+      (TranslatorCommon.is_expr_blank this) ||
+      (TranslatorCommon.is_expr_eq this prefix_expr)
+    );
+    this <- prefix_expr;
+    match ExprMap.find_opt h table with
+    | Some entry -> entry#add prefix_list suffix_list value
+    | None -> begin
+      let new_entry = new data_tracker in
+      new_entry#add_data_update prefix_list suffix_list value root_entry s s';
+      self#add_helper h new_entry
+    end
+  end
 
 end ;;
 
--- lib/translatorCommon.ml
+++ lib/translatorCommon.ml
@@ -1,9 +1,52 @@
 open Syntax
 open Printer
+open Internal
+
+
+(**
+  *  
+  *
+  *)
 
 module TranslatorCommon (M : MetaData) = struct 
   module AST = AST(M)
   module Printer = PrettyPrinter(M)
+
+  module ExprTypeHelper = struct 
+    let expr_to_suffix (e : AST.Prog.expr_t) = 
+      match e with
+      | Suffixed (x, suffix) -> (x, suffix)
+      | _ -> assert false
+
+
+    let suffix_to_dot_id (x : AST.Prog.suffix_t) = 
+      match x with 
+      | AugDot augmented_dotsuffix -> begin
+        let dotsuffix, _ = augmented_dotsuffix in
+        match dotsuffix with 
+        | DSId id -> AST.Prog.NameSeg(id, [])
+        | _ -> assert false
+      end
+      | _ -> assert false
+    
+    let is_expr_tp_aug_dot (e : AST.Prog.expr_t) = 
+      match e with 
+      | Suffixed (_, suffix) -> begin
+        match suffix with
+        | AugDot _ -> true
+        | _ -> false
+      end
+      | _ -> false
+    
+    let is_expr_tp_data_update (e : AST.Prog.expr_t) = 
+      match e with 
+      | Suffixed (_, suffix) -> begin
+        match suffix with
+        | DataUpd _ -> true
+        | _ -> false
+      end
+      | _ -> false
+  end
 
   let str_of_expr (e : AST.Prog.expr_t) : string = 
     Printer.Prog.(print_expr_in_one_line e)
@@ -21,5 +64,48 @@ module TranslatorCommon (M : MetaData) = struct
   let is_expr_blank e = 
     is_expr_eq e expr_blank
 
-end
+  let is_expr_n_blank e = 
+    not (is_expr_eq e expr_blank)
 
+  let expr_to_id (e : AST.Prog.expr_t) : id_t = 
+    match e with 
+    | NameSeg name_seg -> begin
+      let id, _ = name_seg in id
+    end
+    | _ -> assert false
+
+  let move_one_expr_from_suffix_to_prefix prefix_list suffix_list =
+    let suffix_list = NonEmptyList.coerce suffix_list in
+    let suffix_list, h = NonEmptyList.unsnoc suffix_list in
+    let prefix_list = prefix_list @ [h] in
+    prefix_list, suffix_list
+  
+  let convert_expr_lst_to_dot_expr
+    (lst : AST.Prog.expr_t list) : AST.Prog.expr_t = 
+    let lst = NonEmptyList.coerce lst in
+    NonEmptyList.fold_left_1 begin
+      fun l r ->
+        (* checking *)
+        let r_id = expr_to_id r in
+        Suffixed (
+          l, 
+          AugDot (DSId(r_id), [])
+        )
+    end lst
+  
+  (* Here the expr occured can only be id *)  
+  let convert_dot_expr_to_expr_lst (x : AST.Prog.expr_t) = 
+    let rec aux x = 
+      match ExprTypeHelper.is_expr_tp_aug_dot x with
+      | true -> begin
+        let x, suffix = ExprTypeHelper.expr_to_suffix(x) in
+        (aux x) @ [ExprTypeHelper.suffix_to_dot_id suffix]
+      end
+      | false -> begin
+        let _ = expr_to_id x in
+        [x]
+      end
+    in
+    aux x
+
+end
