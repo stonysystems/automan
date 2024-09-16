72136d4bb16c43f1435aebc713b260630f473682
Christa Jenkins <cwjnkins@gmail.com>
2024-09-16 16:09:33 -0500

    test: add initial test_moder
    Signed-off-by: Christa Jenkins <cwjnkins@gmail.com>

---
+++ test/test_moder.ml
@@ -0,0 +1,46 @@
+open Automan
+open Core
+open Lexing
+open TestCommon
+
+let main dafny_fn automan_fn () =
+  (* Dafny *)
+  let dafny = begin
+    let inx = In_channel.create dafny_fn in
+    let lexbuf = Lexing.from_channel inx in
+    lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = dafny_fn };
+    let dafny = parse_dafny_with_error lexbuf in
+    In_channel.close inx;
+    dafny
+  end in
+  let ann = begin
+    let inx = In_channel.create automan_fn in
+    let lexbuf = Lexing.from_channel inx in
+    lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = automan_fn };
+    let ann = parse_annotations_with_error lexbuf in
+    In_channel.close inx;
+    ann
+  end in
+  match Annotator.annotate ann dafny with
+  | Result.Error msg ->
+    printf "Error: %s\n" msg
+  | Result.Ok    dfy ->
+    let (dfy_moded, log) = Moder.run dfy in
+    printf
+      ">>>> BEGIN ERROR LOG >>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n%s\n<<<< END ERROR LOG <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n"
+      (Internal.List.show
+         Moder.(pp_error_t pp_error_mode_expr_t)
+         log);
+    printf "%s\n" Moder.ModePass.(show dfy_moded);
+    ()
+
+let () =
+  Command.basic_spec
+    ~summary:"Run annotator on Dafny specification and Automan annotation file, printing the resulting AST"
+    Command.Spec.(
+      empty
+      +> anon ("dafnyFilename" %: string)
+      +> anon ("automanFilename" %: string)
+    )
+    main
+  |> Command_unix.run
